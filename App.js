06436fb52944ad6978679cc01096502fa6d797f1
matheus <tavaresmat@poli.ufrj.br>
2020-05-10 13:04:09 -0300

    começo App com React-Native

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, View, StyleSheet, Image } from "react-native";
+import * as screen from "./src/constants/dimesions.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+export default function App(){
+  return(
+    <SafeAreaView style = {styles.container}>
+      <View style = {styles.header}>
+        <Image 
+          source = {require("./src/assets/images/covidTitle.png")} 
+          style = {styles.titleImage}
+          resizeMode = {"contain"} 
+        />
+      </View>
+      <View style = {styles.gifContainer}>
+        <Image
+          source = {require("./src/assets/images/corona-doctor.gif")}
+          style = {styles.gifImage}
+          resizeMode = {"cover"}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(
+  {
+    container:{
+      flex: 1,
+    },
+    header:{
+      height: screen.height * 0.3,
+      width: screen.width * 1,
+      backgroundColor: "red",
+      marginTop: screen.height * 0.05,
+      alignItems: "flex-end",
+    },
+    titleImage:{
+      width: screen.width * 0.8,
+      height: screen.height * 0.3,
+      marginRight: screen.width * 0.05
+    },
+    gifContainer:{
+      width: screen.width,
+      height: screen.height * 0.4,
+      backgroundColor: "yellow",
+    },
+    gifImage:{
+      width: screen.width,
+      height: screen.height * 0.4,
+    },
+  }
+);
