dea93d764f854630ca42ed7cc69e307fc5ab5758
Matheus Felinto <38055752+tavaresmat@users.noreply.github.com>
2020-07-06 02:37:27 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://app-covid-fluxo.herokuapp.com/"
+    baseURL: "https://fluxofullstack.herokuapp.com/"
 });
 
 export default api;
